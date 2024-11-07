d073b1920731af535b4c42a7683a88ecfcbf656f
deephikamaths@gmail.com <deephikamaths@gmail.com>
2024-11-07 14:35:22 +0530

    change

---
+++ day-3 NodeJS/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { dbConnect } = require("./shared/db");
+const studentRoute = require("./Routes/student");
+const mentorRoute = require("./Routes/mentor");
+const assignMentortoStudent = require("./Routes/assignMentortoStudent");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Working fine...");
+});
+app.use("/student", studentRoute);
+app.use("/mentor", mentorRoute);
+app.use("/assignmentor", assignMentortoStudent);
+
+app.listen(process.env.PORT || 3000, async (err) => {
+  await dbConnect();
+  console.log("Started server ");
+  if (err) {
+    console.log(err, "error in starting server");
+  }
+});
